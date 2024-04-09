d422b0b61f8118ffad13858214c441b9efc296bd
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-04-09 21:21:25 +0530

    Add files via upload

---
+++ API_Avatar_Generator/apiImage.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   let name= $("#name").val();
+   $("#change").click(
+    function(){
+
+        $("#avatar").attr("src","https://joesch.moe/api/v1/" + name);
+        // $("#name").val('');
+    }
+   )
+
+   $("#reset").click(
+    function(){
+
+        $("#avatar").attr("src","fraud2.png");
+        $("#name").val('');
+    }
+   )
+})
